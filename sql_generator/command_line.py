6d1a9a49d88000347f51fba1ed3e1fb9ce5e4ee1
Janko Ljubic <ljubicjanko1 AT gmail DOT com>
2021-03-15 20:23:53 +0100

    Command line arguments extended, main file refactored

---
+++ sql_generator/command_line.py
@@ -0,0 +1,43 @@
+from os.path import dirname, join
+from utils import str2bool
+import argparse
+
+
+class CommandLine:
+    def __init__(self):
+        this_folder = dirname(__file__)
+
+        defaultSqlOutput = join(join(this_folder, 'srcgen'), "create_db_schema.sql")
+        defaultDotOutput = join(join(this_folder, 'srcgen'), 'er_diagram.dot')
+
+        parser = argparse.ArgumentParser(description='Process file locations and types.')
+        parser.add_argument('-srcSg', '--sourceSgFile',
+                            action="store", dest="sourceFile",
+                            help="parh to file where source sg script is stored")
+        parser.add_argument('-sql', '--sqlOutputPath',
+                            action="store", dest="sqlFile",
+                            help="path to file where generated sql script should be stored", default=defaultSqlOutput)
+        parser.add_argument('-dot', '--dotOutputPath',
+                            action="store", dest="dotFile",
+                            help="path to file where generated dot file should be stored", default=defaultDotOutput)
+        parser.add_argument("--dot-only", type=str2bool, nargs='?',
+                            const=True, default=False, action="store", dest="dotOnly",
+                            help="Specify if you only want dot output file.")
+        parser.add_argument("--sql-only", type=str2bool, nargs='?',
+                            const=True, default=False, action="store", dest="sqlOnly",
+                            help="Specify if you only want sql output file.")
+        self.args = parser.parse_args()
+
+        if self.args.sourceFile:
+            print("You have used '-srcSg' or '--sourceSgFile' with argument: {0}".format(self.args.sourceFile))
+        if self.args.sqlFile:
+            print("You have used '-sql' or '--sqlOutputPath' with argument: {0}".format(self.args.sqlFile))
+        if self.args.dotFile:
+            print("You have used '-dot' or '--dotOutputPath' with argument: {0}".format(self.args.dotFile))
+        if self.args.dotOnly:
+            print("You have used '--dot-only' with argument: {0}".format(self.args.dotOnly))
+        if self.args.sqlOnly:
+            print("You have used '--sql-only' with argument: {0}".format(self.args.sqlOnly))
+        if self.args.dotOnly and self.args.sqlOnly:
+            raise argparse.ArgumentTypeError("You can not use '--dot-only' and '--sql-only' together. Use eighter one or none")
+
---
+++ sql_generator/relations_manager.py
@@ -0,0 +1,56 @@
+from models import Entity, Property, Relation, ONE_TO_MANY, MANY_TO_MANY
+from utils import get_current_time, find_pk_property, find_entity
+
+
+def manage_relations(structure, entities):
+    '''
+    Method used to manage all the manyTomany, oneToMany Todo: ManyToOne
+    properties of a structure by removing them and creating inter table if needed
+    '''
+    non_relation_properties = []
+
+    for property_from_structure in structure.properties:
+        if property_from_structure.manyToMany is not None:
+            '''
+            Handling many to many property
+            '''
+            ftProp = find_pk_property(structure.properties)
+            stProp = find_pk_property(property_from_structure.manyToMany.properties)
+
+            ftEntityName = structure.name
+            stEntityName = property_from_structure.manyToMany.name
+
+            entity = Entity(structure.name + "_" + property_from_structure.manyToMany.name)
+            # idProperty = Property('id', 'number')
+            # idProperty.constraints.append('PRIMARY KEY')
+            # entity.properties.append(idProperty)
+
+            firstRelation = Relation(f'{ftEntityName}_{ftProp.name}'.lower(), ftProp.type, ftEntityName, ftProp.name,
+                                     MANY_TO_MANY)
+            entity.add_relation(firstRelation)
+
+            secondRelation = Relation(f'{stEntityName}_{stProp.name}'.lower(), stProp.type, stEntityName, stProp.name,
+                                      MANY_TO_MANY)
+            entity.add_relation(secondRelation)
+
+            entities.append(entity)
+
+        elif property_from_structure.oneToMany is not None:
+            main_entity = find_entity(structure.name, entities)
+            related_entity = find_entity(property_from_structure.oneToMany.name, entities)
+
+            main_entity_pk_property = find_pk_property(structure.properties)
+            name = f'{main_entity.name}_{main_entity_pk_property.name}_{property_from_structure.name}'.lower()
+
+            relation = Relation(name, main_entity_pk_property.type, main_entity.name, main_entity_pk_property.name,
+                                ONE_TO_MANY)
+            related_entity.add_relation(relation)
+
+        elif property_from_structure.oneToOne is not None:
+            # Todo: implement many to one relation
+            pass
+
+        elif property_from_structure not in non_relation_properties:
+            non_relation_properties.append(property_from_structure)
+
+    return non_relation_properties
